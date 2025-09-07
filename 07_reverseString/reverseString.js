c750651a56411d13955d702f5affeae26734ccd8
FaseehNovice <faseehurrehman499@gmail.com>
2025-09-07 00:43:15 -0700

    complete exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const myArray = string.split("");
+    const reversedArray = []
+    const size = myArray.length
+    for(let i = size ; i >= 0 ; i--){
+        reversedArray.push(myArray.pop());
+    }
+    return reversedArray.join("");
 };
 
 // Do not edit below this line
