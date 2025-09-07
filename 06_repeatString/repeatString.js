fd79dd745745d930c97a5803f1dc8bf173b1f0e3
FaseehNovice <faseehurrehman499@gmail.com>
2025-09-07 00:33:07 -0700

    complete exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string , num) {
+    let i = 1;
+    let container = "ERROR";
+    if(num>=0){
+        container = '';
+        while(i <= num){
+            container += string;
+            i++;
+        }
+    }
+    return container;
 };
 
 // Do not edit below this line
