a3335c5178d1e0f00e7095a01d766f8e870eed9d
FaseehNovice <faseehurrehman499@gmail.com>
2025-09-07 01:39:28 -0700

    complete exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = 0;
+    let max = 0;
+    let min = 0;
+    if( Number.isInteger(a) && Number.isInteger(b) && a > 0 && b > 0){
+        a > b ? max = a: max = b;
+        a > b ? min = b : min = a;
+        for(let i = min; i <= max; i++){
+            sum+=i;
+        }
+        return sum;
+    }
+    else
+        return "ERROR"
 };
 
 // Do not edit below this line
