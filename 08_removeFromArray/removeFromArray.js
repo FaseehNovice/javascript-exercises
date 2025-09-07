e3523b54773315a2b94ea0a29db588141f435a0e
FaseehNovice <faseehurrehman499@gmail.com>
2025-09-07 01:22:00 -0700

    complete exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let numberOfArgs = arguments.length;
+    let count;
+    let size = array.length;
+    for(let i = 0 ; i <  size ; i++){
+        count = 0;
+        while(count <= numberOfArgs - 1){
+            if(array[i] === arguments[count]){
+                array.splice(i,1);
+                i--;
+            }
+            count++;
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
