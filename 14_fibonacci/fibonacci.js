28a71177222f8d47215ea6d1d1c6fbe566220d28
FaseehNovice <faseehurrehman499@gmail.com>
2025-09-13 03:08:53 -0700

    complete exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let toIntInput = parseInt(input) , i = 1 , prevNum = 1 , Num = 1 , fibNum = 0;
+    if( toIntInput < 0)
+        return "OOPS";
+    if(toIntInput == 2)
+        return 1;
+
+    while( i < toIntInput - 1){
+        fibNum = prevNum + Num ;
+        prevNum = Num;
+        Num = fibNum;
+        i++;
+    }
+    if(toIntInput == 0 || toIntInput == 1)
+        return toIntInput;
+
+    return fibNum;
 
 };
 
