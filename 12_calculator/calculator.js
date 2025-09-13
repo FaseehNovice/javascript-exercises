d85aabd07a4523b5cfb89b514749550f0820e051
FaseehNovice <faseehurrehman499@gmail.com>
2025-09-13 02:33:38 -0700

    complete exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,54 @@
 const add = function() {
-	
+	let argNo = arguments.length;
+  let i = 0,sum = 0;
+  while( i < argNo){
+    sum += arguments[i];
+    i++;
+  }
+  return sum;
 };
 
 const subtract = function() {
+  let argNo = arguments.length;
+  let i = 1,subtracted = arguments[0];
+  while( i < argNo){
+    subtracted -= arguments[i];
+    i++;
+  }
+  return subtracted;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  if(array.length == 0)
+    return 0;
+  return array.reduce((acc,item) => acc + item , 0)
 	
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, num) => acc * num ,1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let i = 0 , power = 1; 
+  while (i < b) {
+    power *= a;
+    i++;
+  }
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a == 0 || a == 1){
+    return 1;
+  }
+  let factorial = 1;
+  while( a > 0){
+    factorial *= a;
+    a--;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
