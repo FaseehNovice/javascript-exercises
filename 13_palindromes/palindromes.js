cb23bc549133e20ca301a6fae6b53bb04ac06ba7
FaseehNovice <faseehurrehman499@gmail.com>
2025-09-13 02:53:55 -0700

    complete exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let tempstr = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .replace(/ /g, "")
+    .toLowerCase();
 
+    let left = 0 , right = tempstr.length - 1;
+    while(left < right){
+        if(tempstr[left] == tempstr[right]){
+            left++;
+            right--;
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
